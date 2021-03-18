038e9f3501630c04fcfa049fd93e677f2011661d
DahianaVallejo <62222655+DahianaVallejo@users.noreply.github.com>
2021-03-17 22:54:46 -0500

    Register and Login form

--- screens/account/Account.js
+++ screens/account/Account.js
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import firebase from 'firebase/app'
+import { useFocusEffect } from '@react-navigation/native'
 
-
-import { isUserLogged } from '../../navigations/utils/actions'
+import { getCurrentUser, isUserLogged } from '../../navigations/utils/actions'
 import UserGuest from './UserGuest'
 import UserLogged from './UserLogged'
 
 export default function Account() {
     const [login, setLogin] = useState(null)
 
-    
-    useEffect(() => {
-        setLogin(isUserLogged())
-    }, [])
-         
+    useFocusEffect (
+        useCallback(() => {
+            const user = getCurrentUser()
+            user ? setLogin(true) : setLogin(false)
+        }, [])
+    )    
 
     if (login == null)  {
         return <Text>Cargando...</Text>
